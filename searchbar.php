13edb5d0874f376cad3843a3f1a97d658fcb6404
soulsoft119 <soulsoft.soul119@gmail.com>
2023-06-12 14:26:19 +0530

    commit

--- searchbar.php
+++ searchbar.php
@@ -1,13 +1,1 @@
-<div class="container mt-3">
-  <div class="row justify-content-center">
-    <div class="col-lg-6">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" aria-label="Search">
-        <button class="btn btn-primary" type="button">
-          <i class="bi bi-search"></i>
-        </button>
-      </div>
-    </div>
-  </div>
-</div>
 
