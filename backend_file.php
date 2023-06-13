dbea476cdf555ded8d52ca4fbcc5cf0470b27246
soulsoft119 <soulsoft.soul119@gmail.com>
2023-06-13 13:14:28 +0530

    session variable added

--- backend_file.php
+++ backend_file.php
@@ -7,7 +7,7 @@
 if (isset($_POST['register'])) {
 
 	$name = $_POST['name'];
-	$username = $_POST['username'];
+	// $username = $_POST['username'];
 	$landmark = $_POST['landmark'];
 	$mob = $_POST['mob'];
 	$email = $_POST['email'];
@@ -18,7 +18,7 @@ if (isset($_POST['register'])) {
 	$currentDate = date('Y-m-d');
 
 
-	$sql = "INSERT INTO `usermaster`( `usertype`, `name`, `contact`, `email`, `password`, `username`, `landmark`, `pincode`, `address`, `registrationdate`) VALUES ('customer','$name','$mob','$email','$encypass','$username','$landmark','$pincode','$address','$currentDate')";
+	$sql = "INSERT INTO `usermaster`( `usertype`, `name`, `contact`, `email`, `password`, `username`, `landmark`, `pincode`, `address`, `registrationdate`) VALUES ('customer','$name','$mob','$email','$encypass','$contact','$landmark','$pincode','$address','$currentDate')";
 	// $sql = "INSERT INTO `usermaster`(`usertype`, `name`, `contact`, `email`, `password`, `username`, `landmark`, `pincode`, `address`, `address`) VALUES ('customer','$name','$mob','$email','$encypass','$username','$landmark','$pincode','$address')";
 
 	if (mysqli_query($con, $sql)) {
@@ -39,9 +39,15 @@ if (isset($_POST['login'])) {
 	$password = $_POST['password'];
 
 
-	$sql = "SELECT `contact`, `user_password` FROM `usermaster` WHERE `contact` = '$mob' AND `password` = '$password'";
+	$sql = "SELECT `contact`, `password` FROM `usermaster` WHERE `contact` = '$mob' AND `password` = '$password'";
+	$result = mysqli_query($con, $sql);
 
-	if (mysqli_query($con, $sql)) {
+	if ($result) {
+
+		$rows = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $rows['username'];
+		$_SESSION['userid'] = $rows['userid'];
+		
 		echo "<script>
 			alert('Login Successful');
 			window.location.href='./header.php';
@@ -49,7 +55,7 @@ if (isset($_POST['login'])) {
 	} else {
 		echo "<script>
 			alert('Credential Error');
-			window.location.href='./loginForm.php';
+			// window.location.href='./loginForm.php';
 			</script>";
 	}
 }
