fc125f5b8f44ec9431fb8753d375f65986ac1465
soulsoft119 <soulsoft.soul119@gmail.com>
2023-06-13 12:20:29 +0530

    prodcuction data added

--- Admin/config.php
+++ Admin/config.php
@@ -3,5 +3,11 @@
 <?php
  $con=mysqli_connect("localhost","root","","rgb_oos")or die(mysqli_error($con));
 
+
+
+ // For production
+
+//  $con=mysqli_connect("localhost","gvsoft_root","gvsoft@2023","gvsoft_rgb_oos")or die(mysqli_error($con));
+
 ?>
 
