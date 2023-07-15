38639b7a66428f6ec4e2249a18adfeb9f6ed5eaf
soulsoft119 <130726821+soulsoft119@users.noreply.github.com>
2023-07-15 14:29:22 -0700

    config change for dev brnach

--- Admin/config.php
+++ Admin/config.php
@@ -16,7 +16,7 @@
 
 //  $con=mysqli_connect("localhost","root","","rgb_oos"); 
 //  $con=mysqli_connect("localhost","rajeshahihotel_root","Rajeshahi@25","rajeshahihotel_rgb_oos"); 
- $con=mysqli_connect("89.117.157.187","u311423116_rajeshahi","JeM2Gj$Ub0","u311423116_Rajeshahi"); 
+ $con=mysqli_connect("89.117.157.187","u311423116_rajeshahi","Rajeshahi@2023","u311423116_Rajeshahi"); 
 
  if ($con) {
      
