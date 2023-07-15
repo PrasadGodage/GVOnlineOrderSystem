ee6088bc55e9d5b7aa60f95eb15c11374ee0be70
soulsoft119 <130726821+soulsoft119@users.noreply.github.com>
2023-07-15 15:53:52 -0700

    update

--- Admin/config.php
+++ Admin/config.php
@@ -16,12 +16,9 @@
 
 //  $con=mysqli_connect("localhost","root","","rgb_oos"); 
 //  $con=mysqli_connect("localhost","rajeshahihotel_root","Rajeshahi@25","rajeshahihotel_rgb_oos"); 
- $con=mysqli_connect("89.117.157.187","u311423116_rajeshahi","Rajeshahi@2023","u311423116_Rajeshahi"); 
+ $con=mysqli_connect("localhost","u311423116_rajeshahi","Rajeshahi@2023","u311423116_Rajeshahi"); 
 
- if ($con) {
-     
-		
-	
+ if ($con) {	
  } else {
 	echo "error";
  }
